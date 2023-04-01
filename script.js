6d827f9e6b608313d5f44747e1b9131403528546
everodriguezval <eve.rodriguezval@gmail.com>
2023-04-01 23:56:53 +0100

    getting elements from the DOM using getElement()

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const searchInput = getElement('#search');
+const btnContainer = getElement('#btn-container');
+const productContainer = getElement('#productslist-container');
+
+console.log(searchInput);
+console.log(btnContainer);
+console.log(productContainer);
+
+// getElement('#h2');
+
+// Grab element from the DOM
+// Argument: a string representation of the element's ID
+function getElement(identifier) {
+    const el = document.querySelector(identifier);
+    if (!el) {
+        throw new Error('Could not find the element specified')
+    } else {
+        return el;
+    }
+}
+
 // Display list of products
 // Argument: An array of products
 function displayProducts(products) {}
@@ -9,10 +30,6 @@ function createBtn(category) {}
 // Handle input
 // Argument: a string representation of a category, used to compare against categories of products
 function filterProducts(value) {}
-
-// Grab element from the DOM
-// Argument: a string representation of the element's ID
-function getElement(identifier) {}
 
 // Display category buttons
 // Argument: An array of products
